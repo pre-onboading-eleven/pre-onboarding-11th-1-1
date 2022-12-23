fb0593f5b73f9edec21f3f2faa820af6f00d27d2
gandy818 <gandy818@gmail.com>
2022-12-23 09:10:43 +0900

    Remove : 불필요한 파일 삭제

---
+++ src/index.jsx
@@ -0,0 +1,24 @@
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Signin from './pages/auth/Signin';
+import Signup from './pages/auth/Signup';
+import Todo from './pages/todo/Todo';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Signin />,
+  },
+  {
+    path: '/signup',
+    element: <Signup />,
+  },
+  {
+    path: '/todo',
+    element: <Todo />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<RouterProvider router={router} />);
